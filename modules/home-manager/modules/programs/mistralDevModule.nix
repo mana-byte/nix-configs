54d211796b47123920cbab7fffc78bcd40ec1d78
manaikilaut@gmail.com <manaikilaut@gmail.com>
2026-06-11 22:10:47 +0200

    feat(mistralDevModule): add new dependencies, uv upgrade

--- modules/home-manager/modules/programs/mistralDevModule.nix
+++ modules/home-manager/modules/programs/mistralDevModule.nix
@@ -9,18 +9,18 @@
     let
       uv0102 = pkgs.uv.overrideAttrs (old: rec {
         inherit (old) pname;
-        version = "0.10.2";
+        version = "0.11.8";
         src = pkgs.fetchFromGitHub {
           owner = "astral-sh";
           repo = "uv";
           tag = version;
-          hash = "sha256-7huzemS9BLCOmfzr2cSd8Tc4PtTJV0peYQ5FN2VaPKw=";
+          hash = "sha256-wu9EA3z/qj29lfSj8wKE4p8XEAJQakQTg2AK8I/64us=";
         };
 
         # See: https://github.com/Mic92/nix-update/issues/474#issuecomment-4149762084
         cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
           inherit src;
-          hash = "sha256-kSpRcliQpCCjpZUSCwd9THszOSmdXDIooJA4ZPtRjvo=";
+          hash = "sha256-ygGRoZgD88Q2EkN0U4SDTwya96Ds3Pqy3Llj8cjGwnY=";
         };
       });
     in
@@ -31,7 +31,6 @@
         pnpm
         fnm
         gnumake
-        python3
         dotenv-cli
         ngrok
         prettier
@@ -39,6 +38,13 @@
         cargo
         azure-cli
         uv0102
+        python3
+        vscode
+        terraform
+        claude-code
+
+        # system python packages
+        mypy
       ];
       programs.zsh = {
         enable = true;
